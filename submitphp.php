b9e079dd80e54a31216d53a95e9185ce863ff203
Gyuhyeon Lee <mark3236@naver.com>
2017-04-17 15:52:38 +0900

    email address update

--- submitphp.php
+++ submitphp.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "depressingmoonshine@gmail.com";
+    $email_subject = "Automatic Email From Website";
  
     function died($error) {
         // your error code can go here
